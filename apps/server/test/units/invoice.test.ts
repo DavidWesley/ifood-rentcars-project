3b8552426afbd4c4a26310ad5caf283ed3227bd7
David Wesley <contato.dwesley@outlook.com>
2023-11-23 00:55:59 -0300

    test(server): create unit tests

---
+++ apps/server/test/units/invoice.test.ts
@@ -0,0 +1,39 @@
+import { LicenseTypeEnum } from "@/models/customer.ts"
+import { InvoiceModel, InvoiceProps } from "@/models/invoice.ts"
+import { beforeEach, describe, expect, it } from "vitest"
+
+describe("InvoiceModel", () => {
+    let invoice: InvoiceModel
+    let invoiceProps: InvoiceProps
+    let baseCustomerProps: BaseCustomerProps
+
+    beforeEach(() => {
+        baseCustomerProps = {
+            name: "John Doe",
+            email: "john@example.com",
+            cpf: "123.456.789-00",
+            licenseType: LicenseTypeEnum.AB,
+            birthDate: new Date("1990-01-01"),
+            gender: "male",
+        }
+
+        invoiceProps = {
+            customerId: customerId,
+            costDetails: "",
+            rentals: [rental],
+        }
+
+        invoice = new InvoiceModel(invoiceProps)
+    })
+
+    it("should create a new invoice", () => {
+        expect(invoice).toBeDefined()
+    })
+
+    it("should have correct properties", () => {
+        expect(invoice.id).toBe(invoiceProps.id)
+        expect(invoice.customerId).toBe(invoiceProps.customerId)
+        expect(invoice.costDetails).toBe(invoiceProps.costDetails)
+        expect(invoice.rentals).toEqual(invoiceProps.rentals)
+    })
+})
