17c6be39ee82719674e2b2eb1c3aad99bd611bf8
RudsonSantana <rudsonsanttana@gmail.com>
2023-11-23 01:29:46 -0300

    fix: committing changes before pull

--- apps/server/src/interfaces/repository.ts
+++ apps/server/src/interfaces/repository.ts
@@ -1,17 +1,15 @@
-import type { UUID } from "node:crypto"
-
-export interface TableModelQuery<M> {
+export interface RepositoryQuery<M> {
     filters: Partial<M>
 }
 
 export interface Repository<M> {
-    create(data: M): Promise<UUID | undefined>
+    create(data: M): Promise<void>
     findAll(): Promise<M[]>
-    findById(id: UUID): Promise<M | null>
-    findOne(filters: TableModelQuery<M>["filters"]): Promise<M | null>
-    update(id: UUID, data: Partial<M>): Promise<void>
-    updateMany(filters: TableModelQuery<M>["filters"], data: Partial<M>): Promise<number>
-    delete(id: UUID): Promise<boolean>
-    deleteMany(filters: TableModelQuery<M>["filters"]): Promise<number>
-    count(filters: TableModelQuery<M>["filters"]): Promise<number>
+    findById(id: string | number): Promise<M | null>
+    findOne(filters: RepositoryQuery<M>["filters"]): Promise<M | null>
+    update(id: string | number, data: Partial<M>): Promise<void>
+    updateMany(filters: RepositoryQuery<M>["filters"], data: Partial<M>): Promise<number>
+    delete(id: string | number): Promise<boolean>
+    deleteMany(filters: RepositoryQuery<M>["filters"]): Promise<number>
+    count(filters: RepositoryQuery<M>["filters"]): Promise<number>
 }
