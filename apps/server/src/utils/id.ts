b0c668de3ae8015cb511a5fbee689484707ae501
David Wesley <contato.dwesley@outlook.com>
2023-11-25 12:34:35 -0300

    refactor: refactor utils exports

--- apps/server/src/utils/id.ts
+++ apps/server/src/utils/id.ts
@@ -6,8 +6,4 @@ const validateUUID = (uuid: string): uuid is UUID => {
     return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(uuid)
 }
 
-export {
-    createUUID,
-    UUID,
-    validateUUID,
-}
+export { createUUID, UUID, validateUUID }
