0296a9c1869924bec0bde5f3e09e65bae1c495d1
David Wesley <contato.dwesley@outlook.com>
2023-11-26 10:44:51 -0300

    feat(vehicle): Add routes and controller methods for retrieving available vehicles by type

--- apps/server/src/controllers/vehicleController.ts
+++ apps/server/src/controllers/vehicleController.ts
@@ -1,6 +1,9 @@
-import { System } from "@/models/system/system.ts"
 import { FastifyReply, FastifyRequest } from "fastify"
 import { StatusCodes } from "http-status-codes"
+
+import { System } from "@/models/system/system.ts"
+import { VehicleTypeEnum } from "@/models/vehicle/vehicle.ts"
+import { vehicleRepository } from "@/repositories/vehicleRepository.ts"
 
 // TODO: Implementar as funcionalidades devidamente para essa controller
 export class VehicleController {
@@ -14,19 +17,52 @@ export class VehicleController {
         return response.status(StatusCodes.OK).send(data)
     }
 
+    public static async getAvailableCars(request: FastifyRequest, response: FastifyReply) {
+        // const data = await CarsRentalSystem.getInstance().listAvailableCars()
+        const data = await vehicleRepository.findAvailableVehiclesByType(VehicleTypeEnum.CAR)
+        return response.status(StatusCodes.OK).send(data)
+    }
+
+    public static async getAvailableMotorcycles(request: FastifyRequest, response: FastifyReply) {
+        // const data = await MotorcyclesRentalSystem.getInstance().listAvailableMotorcycles()
+        const data = await vehicleRepository.findAvailableVehiclesByType(VehicleTypeEnum.MOTORCYCLE)
+        return response.status(StatusCodes.OK).send(data)
+    }
+
+    public static async getAvailableBuses(request: FastifyRequest, response: FastifyReply) {
+        // const data = await BusesRentalSystem.getInstance().listAvailableBuses()
+        const data = await vehicleRepository.findAvailableVehiclesByType(VehicleTypeEnum.BUS)
+        return response.status(StatusCodes.OK).send(data)
+    }
+
     public static async getRentedVehicles(request: FastifyRequest, response: FastifyReply) {
         const data = await System.getInstance().listRentedVehicles()
         return response.status(StatusCodes.OK).send(data)
     }
 
     public static async createVehicle(request: FastifyRequest, response: FastifyReply) {
-        await System.getInstance().addVehicle(request.body)
-        return response.status(StatusCodes.CREATED)
+        const vehicleData = request.body.vehicle
+
+        try {
+            await System.getInstance().addVehicle(vehicleData)
+        } catch (err) {
+            if (err instanceof Error) return response.status(StatusCodes.CONFLICT).send({ message: err.message })
+        }
+
+        return response.status(StatusCodes.CREATED).send()
     }
 
     public static async excludeVehicle(request: FastifyRequest, response: FastifyReply) {
         const { plate } = request.params
-        await System.getInstance().removeVehicle(plate)
-        return response.status(StatusCodes.OK)
+
+        try {
+            await System.getInstance().removeVehicle(plate)
+        } catch (err) {
+            // TODO:
+            // Melhorar quais instâncias de erros podem ocorrer
+            if (err instanceof Error) return response.status(StatusCodes.CONFLICT).send({ message: err.message })
+        }
+
+        return response.status(StatusCodes.NO_CONTENT).send()
     }
 }
--- apps/server/src/routes/vehicleRoutes.ts
+++ apps/server/src/routes/vehicleRoutes.ts
@@ -5,7 +5,13 @@ import { VehicleController } from "@/controllers/vehicleController.ts"
 export const vehicleRoutes = async (app: FastifyInstance) => {
     app.get("/", VehicleController.getVehicles)
     app.post("/", VehicleController.createVehicle)
+
     app.get("/available", VehicleController.getAvailableVehicles)
+    app.get("/available/car", VehicleController.getAvailableCars)
+    app.get("/available/motorcycle", VehicleController.getAvailableMotorcycles)
+    app.get("/available/bus", VehicleController.getAvailableBuses)
+
     app.get("/rented", VehicleController.getRentedVehicles)
+
     app.delete("/:plate", VehicleController.excludeVehicle)
 }
