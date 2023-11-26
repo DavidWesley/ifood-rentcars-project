6da30ae9fe9e00eaf377f3759255d2cb706a22ee
David Wesley <contato.dwesley@outlook.com>
2023-11-25 22:29:04 -0300

    refactor(rentalCarSystem): Commented out unused imports and methods

--- apps/server/src/models/system/rentalCarSystem.ts
+++ apps/server/src/models/system/rentalCarSystem.ts
@@ -1,58 +1,58 @@
-import { CustomerModel } from "@/models/customer/customer.ts"
-import { InvoiceModel } from "@/models/invoice/invoice.ts"
-import { RentalModel } from "@/models/rental/rental.ts"
-import { System } from "@/models/system/system.ts"
-import { BaseCarProps, Car } from "@/models/vehicle/car.ts"
-import { CustomerRepositoryInterface } from "@/repositories/customerRepository.ts"
-import { InvoiceRepositoryInterface } from "@/repositories/invoiceRepository.ts"
-import { RentalRepositoryInterface } from "@/repositories/rentalRepository.ts"
-import { VehicleRepositoryInterface } from "@/repositories/vehicleRepository.ts"
+// import { CustomerModel } from "@/models/customer/customer.ts"
+// import { InvoiceModel } from "@/models/invoice/invoice.ts"
+// import { RentalModel } from "@/models/rental/rental.ts"
+// import { System } from "@/models/system/system.ts"
+// import { BaseCarProps, Car } from "@/models/vehicle/car.ts"
+// import { CustomerRepositoryInterface } from "@/repositories/customerRepository.ts"
+// import { InvoiceRepositoryInterface } from "@/repositories/invoiceRepository.ts"
+// import { RentalRepositoryInterface } from "@/repositories/rentalRepository.ts"
+// import { VehicleRepositoryInterface } from "@/repositories/vehicleRepository.ts"
 
-export class RentalCarsSystem extends System {
-    private constructor(
-        customerRepo: CustomerRepositoryInterface<CustomerModel>,
-        carRepo: VehicleRepositoryInterface<Car>,
-        rentalRepo: RentalRepositoryInterface<RentalModel>,
-        invoiceRepo: InvoiceRepositoryInterface<InvoiceModel>
-    ) {
-        super(customerRepo, carRepo, rentalRepo, invoiceRepo)
-    }
+// export class RentalCarsSystem extends System {
+//     private constructor(
+//         customerRepo: CustomerRepositoryInterface<CustomerModel>,
+//         carRepo: VehicleRepositoryInterface<Car>,
+//         rentalRepo: RentalRepositoryInterface<RentalModel>,
+//         invoiceRepo: InvoiceRepositoryInterface<InvoiceModel>
+//     ) {
+//         super(customerRepo, carRepo, rentalRepo, invoiceRepo)
+//     }
 
-    async addCar(props: BaseCarProps): Promise<void> {
-        if ((await this.vehicleRepo.findByPlate(props.plate)) !== null) {
-            throw new Error(`Already exists car with this plate: ${props.plate}`)
-        }
+//     async addCar(props: BaseCarProps): Promise<void> {
+//         if ((await this.vehicleRepo.findByPlate(props.plate)) !== null) {
+//             throw new Error(`Already exists car with this plate: ${props.plate}`)
+//         }
 
-        const car = new Car(props)
+//         const car = new Car(props)
 
-        this.vehicleRepo.create(car)
-    }
+//         this.vehicleRepo.create(car)
+//     }
 
-    async rentCar(customerId: string, vehicleId: string, startDate: Date, endDate: Date): Promise<boolean> {
-        const customer = await this.customerRepo.findById(customerId)
-        if (customer === null) throw new Error(`Invalid Customer`)
+//     async rentCar(customerId: string, vehicleId: string, startDate: Date, endDate: Date): Promise<boolean> {
+//         const customer = await this.customerRepo.findById(customerId)
+//         if (customer === null) throw new Error(`Invalid Customer`)
 
-        this.rentalRepo.findOne({ customerId: customer.id, state: "active" })
+//         this.rentalRepo.findOne({ customerId: customer.id, state: "active" })
 
-        const car = await this.vehicleRepo.findById(vehicleId)
-        if (car === null) throw new Error(`Invalid Car`)
+//         const car = await this.vehicleRepo.findById(vehicleId)
+//         if (car === null) throw new Error(`Invalid Car`)
 
-        if (customer.licenseType.includes("B") === false) throw new Error("Cliente não pode alugar o carro porque não tem habilitação necessária")
-        if (startDate >= endDate) throw new Error("Invalid Dates")
-        if (car.isAvailable() === false) return false
+//         if (customer.license.includes("B") === false) throw new Error("Cliente não pode alugar o carro porque não tem habilitação necessária")
+//         if (startDate >= endDate) throw new Error("Invalid Dates")
+//         if (car.isAvailable() === false) return false
 
-        // Lógica do aluguel de carros
-        this.vehicleRepo.update(vehicleId, { available: false, popularity: car.popularity + 1 })
-        this.customerRepo.update()
-    }
+//         // Lógica do aluguel de carros
+//         this.vehicleRepo.update(vehicleId, { available: false, popularity: car.popularity + 1 })
+//         this.customerRepo.update()
+//     }
 
-    returnCar(rentalId: string): void {}
+//     returnCar(rentalId: string): void {}
 
-    listAvailableCars(): Car[] {}
+//     listAvailableCars(): Car[] {}
 
-    listRentedCars(): Car[] {}
+//     listRentedCars(): Car[] {}
 
-    generateInvoice(customerId: string): void {}
+//     generateInvoice(customerId: string): void {}
 
-    exitSystem(): void {}
-}
+//     exitSystem(): void {}
+// }
--- apps/server/src/models/system/rentalMotorcycleSystem.ts
+++ apps/server/src/models/system/rentalMotorcycleSystem.ts
@@ -1,34 +1,34 @@
-import { CustomerModel } from "@/models/customer/customer.ts"
-import { InvoiceModel } from "@/models/invoice/invoice.ts"
-import { RentalModel } from "@/models/rental/rental.ts"
-import { System } from "@/models/system/system.ts"
-import { BaseMotorcycleProps, Motorcycle } from "@/models/vehicle/motorcycle.ts"
-import { CustomerRepositoryInterface } from "@/repositories/customerRepository.ts"
-import { InvoiceRepositoryInterface } from "@/repositories/invoiceRepository.ts"
-import { RentalRepositoryInterface } from "@/repositories/rentalRepository.ts"
-import { VehicleRepositoryInterface } from "@/repositories/vehicleRepository.ts"
+// import { CustomerModel } from "@/models/customer/customer.ts"
+// import { InvoiceModel } from "@/models/invoice/invoice.ts"
+// import { RentalModel } from "@/models/rental/rental.ts"
+// import { System } from "@/models/system/system.ts"
+// import { BaseMotorcycleProps, Motorcycle } from "@/models/vehicle/motorcycle.ts"
+// import { CustomerRepositoryInterface } from "@/repositories/customerRepository.ts"
+// import { InvoiceRepositoryInterface } from "@/repositories/invoiceRepository.ts"
+// import { RentalRepositoryInterface } from "@/repositories/rentalRepository.ts"
+// import { VehicleRepositoryInterface } from "@/repositories/vehicleRepository.ts"
 
-export class RentalMotorcyclesSystem extends System {
-    private constructor(
-        customerRepo: CustomerRepositoryInterface<CustomerModel>,
-        motorcycleRepo: VehicleRepositoryInterface<Motorcycle>,
-        rentalRepo: RentalRepositoryInterface<RentalModel>,
-        invoiceRepo: InvoiceRepositoryInterface<InvoiceModel>
-    ) {
-        super(customerRepo, motorcycleRepo, rentalRepo, invoiceRepo)
-    }
+// export class RentalMotorcyclesSystem extends System {
+//     private constructor(
+//         customerRepo: CustomerRepositoryInterface<CustomerModel>,
+//         motorcycleRepo: VehicleRepositoryInterface<Motorcycle>,
+//         rentalRepo: RentalRepositoryInterface<RentalModel>,
+//         invoiceRepo: InvoiceRepositoryInterface<InvoiceModel>
+//     ) {
+//         super(customerRepo, motorcycleRepo, rentalRepo, invoiceRepo)
+//     }
 
-    addMotorcycle(motorcycleDetails: BaseMotorcycleProps): void {}
+//     addMotorcycle(motorcycleDetails: BaseMotorcycleProps): void {}
 
-    rentMotorcycle(customerId: string, vehicleId: string, startDate: Date, endDate: Date): void {}
+//     rentMotorcycle(customerId: string, vehicleId: string, startDate: Date, endDate: Date): void {}
 
-    returnMotorcycle(rentalId: string): void {}
+//     returnMotorcycle(rentalId: string): void {}
 
-    listAvailableMotorcycles(): Motorcycle[] {}
+//     listAvailableMotorcycles(): Motorcycle[] {}
 
-    listRentedMotorcycles(): Motorcycle[] {}
+//     listRentedMotorcycles(): Motorcycle[] {}
 
-    generateInvoice(customerId: string): void {}
+//     generateInvoice(customerId: string): void {}
 
-    exitSystem(): void {}
-}
+//     exitSystem(): void {}
+// }
